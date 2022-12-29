0d0275ce13cdb54ebe427a1a2b775ab6ebc35acc
barsu5489 <ebarsulai@gmail.com>
2022-12-29 08:48:41 +0300

    Start of Header component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  const name = ' Barsu'
+  return (
+    <div className="container">
+   <h1>Hello from React</h1>
+   <h2>Hello {name}</h2> 
+    </div>
+  );
+}
+
+export default App;
