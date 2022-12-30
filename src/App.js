c8e8502b3b257cb126278cd549fd72c22ec34300
barsu5489 <ebarsulai@gmail.com>
2022-12-30 07:54:21 +0300

    task remonder

--- src/App.js
+++ src/App.js
@@ -16,10 +16,23 @@ function App() {
       "reminder": true
     }
   ])
+  //DElete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task)=>task.id !== id))
+  }
+  // Tooge Reminder
+
+  const toggleReminder = (id) => {
+    console.log(id)
+  }
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks = {tasks}/>
+      {tasks.length > 0 ? (
+      <Tasks tasks = {tasks} onDelete = {deleteTask}  onToggle = {toggleReminder}/>
+      ) :(
+         'No Tasks to show'
+         )}
     </div>
   );
 }
